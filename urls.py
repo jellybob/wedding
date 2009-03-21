e31020cbb668715e7c55fc687776016a8c9df777
Jon Wood <jon@McJon.local>
2009-03-21 22:30:42 +0000

    Activate the admin app

--- urls.py
+++ urls.py
@@ -1,19 +1,14 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
+from django.contrib import admin
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^$', include('wedding.home.urls')),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/(.*)', admin.site.root),
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/(.*)', admin.site.root),
 )
 
 if settings.DEBUG:
