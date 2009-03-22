88e0823b410b91e7bf6b2f9f569efa4cef8173cb
Jon Wood <jon@McJon.local>
2009-03-22 20:40:25 +0000

    Add a category filter for gifts.

--- gifts/admin.py
+++ gifts/admin.py
@@ -4,7 +4,7 @@ from wedding.gifts.models import Gift, Category
 class GiftAdmin(admin.ModelAdmin):
     list_display = ("name", "price_in_pounds", "reserved")
     prepopulated_fields = { "slug": ("name",) }
-    list_filter = ("reserved",)
+    list_filter = ("category", "reserved",)
 admin.site.register(Gift, GiftAdmin)
 
 admin.site.register(Category)
