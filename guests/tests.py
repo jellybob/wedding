34d1bb5e43c1218ac3393bf7145744f457e3f5ad
Jon Wood <jon@McJon.local>
2009-03-22 13:15:24 +0000

    Use a second fixture for the guest who isn't invited to the meal

--- guests/tests.py
+++ guests/tests.py
@@ -4,9 +4,8 @@ from guests.models import Guest
 class RsvpTest(TestCase):
     fixtures = ['rsvp_test.json']
     
-    def loadGuest(self):
-        self.guest = Guest.objects.get(pk=1)
-    
+    def loadGuest(self, id):
+        self.guest = Guest.objects.get(pk=id)
     def assertEmailWasInvalid(self, response):
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'guests/index.html')
@@ -37,17 +36,13 @@ class RsvpTest(TestCase):
         self.assertEmailWasInvalid(response)
     
     def testStage2WithValidEmailAndGuestInvitedToMeal(self):
-        self.loadGuest()
-        self.guest.invite_to_meal = True
-        self.guest.save()
+        self.loadGuest(1)
         
         response = self.assertStage2Succeeded()        
         self.assertContains(response, 'Meal', 1)
     
     def testStage2WithValidEmailAndGuestNotInvitedToMeal(self):
-        self.loadGuest()
-        self.guest.invite_to_meal = False
-        self.guest.save()
+        self.loadGuest(2)
         
         response = self.assertStage2Succeeded()
         self.assertNotContains(response, 'Meal')
@@ -57,11 +52,11 @@ class RsvpTest(TestCase):
         self.assertEmailWasInvalid(response)
         
     def testSaveWithValidEmail(self):
-        self.loadGuest()
+        self.loadGuest(1)
         response = self.client.post('/rsvp/save', { 'email': self.guest.email, 'ceremony': '1', 'meal': '0', 'reception': '1' })
         
         # Reload the guest to get current state
-        self.loadGuest()
+        self.loadGuest(1)
         
         self.assertRedirects(response, '/rsvp/confirmed')
         self.assertEquals(self.guest.attending_ceremony, True)
