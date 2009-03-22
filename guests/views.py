7c35f529ce9e926e15d9cee65d4519aafc1c7b81
Jon Wood <jon@McJon.local>
2009-03-22 20:31:20 +0000

    Group guests so that a single contact can RSVP for people individually

--- guests/views.py
+++ guests/views.py
@@ -1,28 +1,32 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
-from wedding.guests.models import Guest
+from wedding.guests.models import Group
 
 def index(request):
     return render_to_response('guests/index.html', { 'error': False })
     
 def stage2(request):
     try:
-        guest = Guest.objects.get(email=request.POST['email'])
-        return render_to_response('guests/stage2.html', { 'guest': guest })
-    except (KeyError, Guest.DoesNotExist):
+        group = Group.objects.get(email=request.POST['email'])
+        guests = group.guest_set.all()
+        return render_to_response('guests/stage2.html', { 'group': group, 'guests': guests })
+    except (KeyError, Group.DoesNotExist):
         return render_to_response('guests/index.html', { 'error': True })
 
 def save(request):
     try:
-        guest = Guest.objects.get(email=request.POST['email'])
-        guest.attending_ceremony = true_or_false(request.POST['ceremony'])
-        guest.attending_meal = true_or_false(request.POST['meal'])
-        guest.attending_reception = true_or_false(request.POST['reception'])
-        guest.save()
-        
+        group = Group.objects.get(email=request.POST['email'])
+        for guest in group.guest_set.all():            
+            guest.attending_ceremony = true_or_false(request.POST['%s_ceremony' % guest.id])
+            guest.attending_meal = true_or_false(request.POST['%s_meal' % guest.id])
+            guest.attending_reception = true_or_false(request.POST['%s_reception' % guest.id])
+            guest.save()
+            
+        group.rsvp_received = True
+        group.save()
         return HttpResponseRedirect(reverse('wedding.guests.views.confirmed'))
-    except (KeyError, Guest.DoesNotExist):
+    except (KeyError, Group.DoesNotExist):
         return render_to_response('guests/index.html', { 'error': True })
         
 def confirmed(request):
