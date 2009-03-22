61ead15502456769343f09b8d90c2d7a47b51a19
Jon Wood <jon@McJon.local>
2009-03-22 01:23:07 +0000

    W00t! RSVPing just about done

--- guests/urls.py
+++ guests/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('wedding.guests.views',
     (r'^$', 'index'),
+    (r'^stage2$', 'stage2'),
     (r'^save$', 'save'),
     (r'^confirmed$', 'confirmed'),
 )
--- guests/views.py
+++ guests/views.py
@@ -1,4 +1,35 @@
 from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from wedding.guests.models import Guest
 
-def index(self):
+def index(request):
     return render_to_response('guests/index.html')
+    
+def stage2(request):
+    try:
+        guest = Guest.objects.get(email=request.POST['email'])
+        return render_to_response('guests/stage2.html', { 'guest': guest })
+    except (KeyError, Guest.DoesNotExist):
+        return render_to_response('guests/index.html', { 'error': True })
+
+def save(request):
+    try:
+        guest = Guest.objects.get(email=request.POST['email'])
+        guest.attending_ceremony = true_or_false(request.POST['ceremony'])
+        guest.attending_meal = true_or_false(request.POST['meal'])
+        guest.attending_reception = true_or_false(request.POST['reception'])
+        guest.save()
+        
+        return HttpResponseRedirect(reverse('wedding.guests.views.confirmed'))
+    except (KeyError, Guest.DoesNotExist):
+        return render_to_response('guests/index.html', { 'error': True })
+        
+def confirmed(request):
+    return render_to_response('guests/confirmed.html')
+    
+def true_or_false(value):
+    if (value == '1'):
+        return True
+    else:
+        return False
